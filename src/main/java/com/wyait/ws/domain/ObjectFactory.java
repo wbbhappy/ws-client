791b178f2e3231009d1d57fc02ff528bea9fb110
wbbhappy <44571086+wbbhappy@users.noreply.github.com>
2019-02-20 17:16:13 +0800

    ws-client

---
+++ src/main/java/com/wyait/ws/domain/ObjectFactory.java
@@ -0,0 +1,20 @@
+package com.wyait.ws.domain;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+@XmlRegistry
+public class ObjectFactory {
+
+    public ObjectFactory() {
+    }
+
+    public GetCountryRequest createGetCountryRequest() {
+        return new GetCountryRequest();
+    }
+    public GetCountryResponse createGetCountryResponse() {
+        return new GetCountryResponse();
+    }
+    public Country createCountry() {
+        return new Country();
+    }
+}
